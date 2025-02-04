d23389844a3dd8fee06c4cdf2d959ef7dbf39025
vineethkumar3 <77826030+vineethkumar3@users.noreply.github.com>
2025-02-04 13:59:13 -0500

    Create variables.tf

---
+++ module/variables.tf
@@ -0,0 +1,21 @@
+variable "resource-group-name" {
+    type=string
+}
+variable "resource-group-location"{
+    type=string
+}
+variable "virtual-network-name" {
+    type=string
+}
+variable "subnet-name"{
+    type=string
+}
+variable "ipadd" {
+  
+}
+variable "net-interface" {
+    type=string
+}
+variable "machine-name"{
+    type=string
+}

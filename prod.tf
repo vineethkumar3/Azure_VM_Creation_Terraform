671814c4efa447f9303385b2c601332fce0e723e
vineethkumar3 <77826030+vineethkumar3@users.noreply.github.com>
2025-02-04 14:00:02 -0500

    Update prod.tf

--- prod.tf
+++ prod.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id="9086104b-c120-4eb7-ad44-64024bb5662b"
-  client_id="5c6014f4-2bd8-4d21-b52f-0a23f421665f"
-  client_secret = "yRj8Q~Wu-iKu1jae0gXEU5xDK3qvLFcm5OQoKdmU"
-  tenant_id = "e1d367c8-fdcf-42d1-a1b8-ec6056e31c91"
+  subscription_id=""
+  client_id=""
+  client_secret = ""
+  tenant_id = ""
 }
 
 
